db1337b6b74b141b9c19d66ef6b7de19fe8d3d33
ann adelsberger <adelsbergerann@gmail.com>
2013-09-27 21:37:04 -0400

    added </html> tag

--- demo.php
+++ demo.php
@@ -1,17 +1,18 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<style>
-    // We'll use this class to style the boxes..Red, 50x50px
-    .box {
-        width:50px;
-        height:50px;
-        float:left;
-        margin:4px;
-        background-color:red;
+	<style>
+    	// We'll use this class to style the boxes..Red, 50x50px
+    	.box {
+        	width:50px;
+        	height:50px;
+        	float:left;
+        	margin:4px;
+        	background-color:red;
     }       
 </style>
 </head>
-<body>
-	<?=$boxes?>
-</body>
+	<body>
+		<?=$boxes?>
+	</body>
+</html>
