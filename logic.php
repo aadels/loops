1d63219dd9169e1e8f4b2b50dcc54dd749faf043
ann adelsberger <adelsbergerann@gmail.com>
2013-09-29 11:32:52 -0400

    added seaparate random ranges for height and width

--- logic.php
+++ logic.php
@@ -1,9 +1,10 @@
 <?php
 $boxes = "";
-$random = rand(50, 300);
-$i = 1;
+$randomw = rand(50, 300);
+$randomh = rand(40, 500);
+$i = 0;
 while ($i < 10){
-	 $boxes = $boxes."<div style='width:{$random}px; height:{$random}px; float:left; margin:4px; 
+	 $boxes = $boxes."<div style='width:{$randomw}px; height:{$randomh}px; float:left; margin:4px; 
 	 				background-color:blue;'>".$i."</div>";
 	 $i++;
 }
