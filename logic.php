e10464756436037e8aa78076d706acaba501557a
ann adelsberger <adelsbergerann@gmail.com>
2013-09-29 11:16:39 -0400

    edited logic style syntax

--- logic.php
+++ logic.php
@@ -1,12 +1,11 @@
 <?php
 $boxes = "";
 $random = rand(50, 300);
-$i= 0;
+$i = 1;
 while ($i < 10){
 	 $boxes = $boxes."<div style='width:{$random}px; height:{$random}px; float:left; margin:4px; 
 	 				background-color:blue;'>".$i."</div>";
 	 $i++;
-
 }
 
 ?>
