f0926bd5a0a63bd3d86c3e40bf481b230bab8eb6
ann adelsberger <adelsbergerann@gmail.com>
2013-09-29 11:00:41 -0400

    added styles to logic page

--- demo.php
+++ demo.php
@@ -5,21 +5,9 @@
 	<meta charset="utf-8">
     <title>Loops</title>
     <?php require_once('logic.php'); ?>
-	<style>
-    	/* We'll use this class to style the boxes..Red, 50x50px */
-    	.box {
-        	width:50px;
-        	height:50px;
-        	float:left;
-        	margin:4px;
-        	background-color:red;
-    	}       
-	</style>
 
 </head>
 <body>
-	<?php
-    echo $boxes rand(0, 9);
-    ?>
+	<?$boxes?>
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,8 +1,10 @@
 <?php
 $boxes = "";
+$random = rand(50, 300);
 $i= 0;
 while ($i < 10){
-	 $boxes = $boxes."<div class='box'>".$i."</div>";
+	 $boxes = $boxes."<div style='width:{$random}px; height:{$random}px; float:left; margin:4px; 
+	 				background-color:blue;'>".$i."</div>";
 	 $i++;
 
 }
