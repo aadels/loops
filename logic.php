8db735e06ddcc040d61c845191936f6fee7b0fc5
ann adelsberger <adelsbergerann@gmail.com>
2013-09-29 11:37:59 -0400

    switched back from while loop sto for loop

--- logic.php
+++ logic.php
@@ -2,11 +2,11 @@
 $boxes = "";
 $randomw = rand(50, 300);
 $randomh = rand(40, 500);
-$i = 0;
-while ($i < 10){
+
+	for($i = 0; $i < 10; $i++) {
 	 $boxes = $boxes."<div style='width:{$randomw}px; height:{$randomh}px; float:left; margin:4px; 
 	 				background-color:blue;'>".$i."</div>";
-	 $i++;
+	 
 }
 
 ?>
