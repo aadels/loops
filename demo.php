25fa3fa3467a7dada0a3b57e9a87aef1815395b8
ann adelsberger <adelsbergerann@gmail.com>
2013-10-02 09:28:52 -0400

    edited style in logic

--- demo.php
+++ demo.php
@@ -5,11 +5,18 @@
 	<meta charset="utf-8">
     <title>Loops</title>
     <?php require_once('logic.php'); ?>
+    <style>
+        .box {
+            width:50px;
+            height:50px;
+            float:left;
+            margin:4px;
+            background-color:red;
+        }       
+    </style>
 
 </head>
 <body>
-	<?php
-	echo $boxes;
-	?>
+	<?=$boxes;?>
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -4,8 +4,7 @@ $randomw = rand(50, 300);
 $randomh = rand(40, 500);
 
 	for($i = 0; $i < 10; $i++) {
-	 $boxes = $boxes."<div style='width:{$randomw}px; height:{$randomh}px; float:left; margin:4px; 
-	 				background-color:blue;'>".$i."</div>";
+	 $boxes = $boxes."<div class='box' style='width".$randomw."px; height:".$randomh."px;'>".$i."</div>";
 	 
 }
 
