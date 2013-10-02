da0868fb78791af438741cd362d5da04f99b024f
ann adelsberger <adelsbergerann@gmail.com>
2013-10-02 11:01:53 -0400

    syntax error

--- logic.php
+++ logic.php
@@ -5,7 +5,7 @@ $randomw = rand(70, 500);
 $randomh = rand(70, 500);
 
 	
-	 $boxes = $boxes."<div class='box' style='width".$randomw."px; height:".$randomh."px;'>".$i."</div>";
+	 $boxes = $boxes."<div class='box' style='width:".$randomw."px; height:".$randomh."px;'>".$i."</div>";
 	 
 }
 
